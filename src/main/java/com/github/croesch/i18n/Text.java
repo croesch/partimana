7e3060c1ad4d0b367726cbe3b6b4df9cb0ed5639
croesch <christianroesch@gmx.net>
2011-06-21 00:08:31 +0200

    Added German language keys for participant attribute names

--- src/main/java/com/github/croesch/i18n/Text.java
+++ src/main/java/com/github/croesch/i18n/Text.java
@@ -8,23 +8,166 @@ import java.util.ResourceBundle;
  * @author $Author: croesch $
  * @version $Revision: 1.9 $ ($Date: 2011/03/07 20:44:01 $)
  */
-@SuppressWarnings("nls")
 public enum Text {
 
   /** the name of the selected language */
   LANGUAGE (0),
+  /** the constant yes */
+  YES (1),
+  /** the constant no */
+  NO (2),
+  /** the constant cancel */
+  CANCEL (3),
+  /** the constant ok */
+  OK (4),
+  /** the constant apply */
+  APPLY (5),
+  /** the constant close */
+  CLOSE (6),
+  /** the constant exit */
+  EXIT (7),
+
+  /** the name for participant */
+  PARTICIPANT (100),
+  /** the name for the id of participant */
+  PARTICIPANT_ID (101),
+  /** the name for the last name of the participant */
+  PARTICIPANT_LASTNAME (102),
+  /** the name for the first name of the participant */
+  PARTICIPANT_FORENAME (103),
+  /** the name for the gender of the participant */
+  PARTICIPANT_GENDER (104),
+  /** the name of the denomination */
+  PARTICIPANT_DENOMINTAION (105),
+  /** the name of the birthday */
+  PARTICIPANT_BIRTHDAY (106),
+  /** the name for living address */
+  PARTICIPANT_ADDRESS_LIVING (107),
+  /** the name for postal address */
+  PARTICIPANT_ADDRESS_POSTAL (108),
+  /** the name for phone number */
+  PARTICIPANT_PHONE (109),
+  /** the name for fax number */
+  PARTICIPANT_FAX (110),
+  /** the name for mobile phone number */
+  PARTICIPANT_MOBILE_PHONE (111),
+  /** the name for phone number of parents */
+  PARTICIPANT_PHONE_OF_PARENTS (112),
+  /** the name for mail address */
+  PARTICIPANT_MAIL_ADDRESS (113),
+  /** the name for county council */
+  PARTICIPANT_COUNTY_COUNCIL (114),
+  /** the name for bank code number */
+  PARTICIPANT_BANK_CODE_NUMBER (115),
+  /** the name for bank */
+  PARTICIPANT_BANK_NAME (116),
+  /** the name for bank account number */
+  PARTICIPANT_BANK_ACCOUNT_NUMBER (117),
+  /** the name for comment */
+  PARTICIPANT_COMMENT (118),
+  /** the name for date since in */
+  PARTICIPANT_DATE_SINCE (119),
+  /** the name for date until in */
+  PARTICIPANT_DATE_UNTIL (120),
+  /** the name for camp participant */
+  PARTICIPANT_CAMP_PARTICIPANT (121),
+  /** the name for staff general */
+  PARTICIPANT_STAFF_GENERAL (122),
+  /** the name for staff youth */
+  PARTICIPANT_STAFF_YOUTH (123),
+  /** the name for board */
+  PARTICIPANT_BOARD (124),
+  /** the name for extended board */
+  PARTICIPANT_EXTENDED_BOARD (125),
+  /** the name for MAK */
+  PARTICIPANT_MAK (126),
+  /** the name for AGE */
+  PARTICIPANT_AGE (127),
+  /** the name for camp kitchen */
+  PARTICIPANT_CAMP_KITCHEN (128),
+  /** the name for seminar */
+  PARTICIPANT_SEMINAR (129),
+  /** the name for misc */
+  PARTICIPANT_MISC (130),
 
   /** the message to show which language is selected */
   DEBUG_SELECTED_LANGUAGE (1000),
-
   /** the message to indicate that program is starting */
   DEBUG_PROGRAM_STARTING (1005),
-
   /** the message to indicate that program is sending the message to exit */
   DEBUG_PROGRAM_EXIT_NOTIFICATION (1009),
-
   /** the message to indicate that program is exiting */
-  DEBUG_PROGRAM_EXITING (1010);
+  DEBUG_PROGRAM_EXITING (1010),
+
+  /** constant for male */
+  MALE (2000),
+  /** constant for female */
+  FEMALE (2001),
+
+  /** constant for evangelic */
+  EVANGELIC (2010),
+  /** constant for catholic */
+  CATHOLIC (2011),
+  /** constant for orthodox */
+  ORTHODOX (2012),
+  /** constant for muslim */
+  MUSLIM (2013),
+  /** constant for free church */
+  FREE_CHURCH (2014),
+  /** constant for jewish */
+  JEWISH (2015),
+  /** constant for other denomination */
+  OTHER_DENOMINATION (2016),
+  /** constant for no denomination */
+  NO_DENOMINATION (2017),
+
+  /** constant for street */
+  STREET (2020),
+  /** constant for post code */
+  POST_CODE (2021),
+  /** constant for city */
+  CITY (2022),
+
+  /** constant for unknown county council */
+  UNKNOWN (2030),
+  /** constant for county of alzey */
+  COUNTY_ALZEY (2031),
+  /** constant for county of bad kreuznach */
+  COUNTY_BAD_KREUZNACH (2032),
+  /** constant for county of bad duerkheim */
+  COUNTY_BAD_DUERKHEIM (2033),
+  /** constant for city of frankenthal */
+  CITY_FRANKENTHAL (2034),
+  /** constant for county of germersheim */
+  COUNTY_GERMERSHEIM (2035),
+  /** constant for city of kaiserslautern */
+  CITY_KAISERSLAUTERN (2036),
+  /** constant for county of kaiserslautern */
+  COUNTY_KAISERSLAUTERN (2037),
+  /** constant for county of kirchheimbolanden */
+  COUNTY_KIRCHHEIMBOLANDEN (2038),
+  /** constant for county of kusel */
+  COUNTY_KUSEL (2039),
+  /** constant for city of landau */
+  CITY_LANDAU (2040),
+  /** constant for city of ludwigshafen */
+  CITY_LUDWIGSHAFEN (2041),
+  /** constant for county of rhein-pfalz */
+  COUNTY_RHEIN_PFALZ (2042),
+  /** constant for city of neustadt */
+  CITY_NEUSTADT (2043),
+  /** constant for city of pirmasens */
+  CITY_PIRMASENS (2044),
+  /** constant for city of speyer */
+  CITY_SPEYER (2045),
+  /** constant for county of suedliche weinstrasse */
+  COUNTY_SUEDLICHE_WEINSTRASSE (2046),
+  /** constant for county of */
+  COUNTY_SUEDWESTPFALZ (2047),
+  /** constant for city of zweibruecken */
+  CITY_ZWEIBRUECKEN (2048),
+  /** constant for another county council */
+  OTHER (2049);
 
   /** the value of this instance */
   private final String string;
