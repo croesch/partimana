bcb21bcee42d1f44c1c065cd0f251a1488bba4d3
croesch <christianroesch@gmx.net>
2011-08-17 23:24:40 +0200

    Changed structure of XMLBundleControl for test purpose and added tests

--- src/main/java/com/github/croesch/i18n/XMLBundleControl.java
+++ src/main/java/com/github/croesch/i18n/XMLBundleControl.java
@@ -72,41 +72,41 @@ final class XMLBundleControl extends Control {
     }
     return bundle;
   }
+}
+
+/**
+ * Resource bundle from javadoc-example of {@link Control} for XML-based bundles.
+ * 
+ * @author croesch
+ * @since Date: Aug 17, 2011
+ */
+final class XMLResourceBundle extends ResourceBundle {
+
+  /** the properties that contain the content of the xml file */
+  private final Properties props;
 
   /**
-   * Resource bundle from javadoc-example of {@link Control} for XML-based bundles.
+   * Constructs the resource bundle from javadoc-example of {@link Control} for XML-based bundles.
    * 
-   * @author croesch
    * @since Date: Aug 17, 2011
+   * @param stream the xml-stream to read the data from
+   * @throws IOException in case of IO problem, or if stream is <code>null</code>
    */
-  private static class XMLResourceBundle extends ResourceBundle {
-
-    /** the properties that contain the content of the xml file */
-    private final Properties props;
-
-    /**
-     * Constructs the resource bundle from javadoc-example of {@link Control} for XML-based bundles.
-     * 
-     * @since Date: Aug 17, 2011
-     * @param stream the xml-stream to read the data from
-     * @throws IOException in case of IO problem, or if stream is <code>null</code>
-     */
-    XMLResourceBundle(final InputStream stream) throws IOException {
-      if (stream == null) {
-        throw new IOException();
-      }
-      this.props = new Properties();
-      this.props.loadFromXML(stream);
+  XMLResourceBundle(final InputStream stream) throws IOException {
+    if (stream == null) {
+      throw new IOException();
     }
+    this.props = new Properties();
+    this.props.loadFromXML(stream);
+  }
 
-    @Override
-    protected Object handleGetObject(final String key) {
-      return this.props.getProperty(key);
-    }
+  @Override
+  protected Object handleGetObject(final String key) {
+    return this.props.getProperty(key);
+  }
 
-    @Override
-    public Enumeration<String> getKeys() {
-      return Collections.enumeration(this.props.stringPropertyNames());
-    }
+  @Override
+  public Enumeration<String> getKeys() {
+    return Collections.enumeration(this.props.stringPropertyNames());
   }
 }
---
+++ src/test/java/com/github/croesch/i18n/XMLBundleControlTest.java
@@ -0,0 +1,181 @@
+package com.github.croesch.i18n;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.junit.Test;
+
+/**
+ * Provides test cases for {@link XMLBundleControl} and {@link XMLResourceBundle}.
+ * 
+ * @author croesch
+ * @since Date: Aug 17, 2011
+ */
+public class XMLBundleControlTest {
+
+  /**
+   * Test method for {@link XMLBundleControl#getFormats(String)}.
+   */
+  @Test(expected = NullPointerException.class)
+  public void testGetFormatsString_NPE() {
+    final XMLBundleControl control = new XMLBundleControl();
+    control.getFormats(null);
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#getFormats(String)}.
+   */
+  @Test
+  public void testGetFormatsString() {
+    final XMLBundleControl control = new XMLBundleControl();
+    assertThat(control.getFormats("")).containsExactly("xml");
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#newBundle(String, Locale, String, ClassLoader, boolean)} .
+   * 
+   * @throws IOException in case of problems
+   * @throws InstantiationException in case of problems
+   * @throws IllegalAccessException in case of problems
+   */
+  @Test(expected = NullPointerException.class)
+  public void testNewBundleStringLocaleStringClassLoaderBoolean_NPE1() throws IllegalAccessException,
+                                                                      InstantiationException, IOException {
+    final XMLBundleControl control = new XMLBundleControl();
+    control.newBundle(null, Locale.getDefault(), "format", ClassLoader.getSystemClassLoader(), true);
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#newBundle(String, Locale, String, ClassLoader, boolean)} .
+   * 
+   * @throws IOException in case of problems
+   * @throws InstantiationException in case of problems
+   * @throws IllegalAccessException in case of problems
+   */
+  @Test(expected = NullPointerException.class)
+  public void testNewBundleStringLocaleStringClassLoaderBoolean_NPE2() throws IllegalAccessException,
+                                                                      InstantiationException, IOException {
+    final XMLBundleControl control = new XMLBundleControl();
+    control.newBundle("baseName", null, "format", ClassLoader.getSystemClassLoader(), true);
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#newBundle(String, Locale, String, ClassLoader, boolean)} .
+   * 
+   * @throws IOException in case of problems
+   * @throws InstantiationException in case of problems
+   * @throws IllegalAccessException in case of problems
+   */
+  @Test(expected = NullPointerException.class)
+  public void testNewBundleStringLocaleStringClassLoaderBoolean_NPE3() throws IllegalAccessException,
+                                                                      InstantiationException, IOException {
+    final XMLBundleControl control = new XMLBundleControl();
+    control.newBundle("baseName", Locale.getDefault(), null, ClassLoader.getSystemClassLoader(), true);
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#newBundle(String, Locale, String, ClassLoader, boolean)} .
+   * 
+   * @throws IOException in case of problems
+   * @throws InstantiationException in case of problems
+   * @throws IllegalAccessException in case of problems
+   */
+  @Test(expected = NullPointerException.class)
+  public void testNewBundleStringLocaleStringClassLoaderBoolean_NPE4() throws IllegalAccessException,
+                                                                      InstantiationException, IOException {
+    final XMLBundleControl control = new XMLBundleControl();
+    control.newBundle("baseName", Locale.getDefault(), "xml", null, true);
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#newBundle(String, Locale, String, ClassLoader, boolean)} .
+   * 
+   * @throws IOException in case of problems
+   * @throws InstantiationException in case of problems
+   * @throws IllegalAccessException in case of problems
+   */
+  @Test
+  public void testNewBundleStringLocaleStringClassLoaderBoolean_Null1() throws IllegalAccessException,
+                                                                       InstantiationException, IOException {
+    final XMLBundleControl control = new XMLBundleControl();
+    assertThat(control.newBundle("baseName", Locale.getDefault(), "XML", ClassLoader.getSystemClassLoader(), true))
+      .isNull();
+  }
+
+  /**
+   * Test method for {@link XMLBundleControl#newBundle(String, Locale, String, ClassLoader, boolean)} .
+   * 
+   * @throws IOException in case of problems
+   * @throws InstantiationException in case of problems
+   * @throws IllegalAccessException in case of problems
+   */
+  @Test
+  public void testNewBundleStringLocaleStringClassLoaderBoolean_Null2() throws IllegalAccessException,
+                                                                       InstantiationException, IOException {
+    final XMLBundleControl control = new XMLBundleControl();
+    assertThat(control.newBundle("baseName", Locale.getDefault(), "xml", ClassLoader.getSystemClassLoader(), true))
+      .isNull();
+  }
+
+  /**
+   * Test method for {@link XMLResourceBundle}.
+   */
+  @Test(expected = MissingResourceException.class)
+  public void testGetString_MRE1() {
+    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/unknown", new XMLBundleControl());
+    bundle.getString("key");
+  }
+
+  /**
+   * Test method for {@link XMLResourceBundle}.
+   */
+  @Test(expected = MissingResourceException.class)
+  public void testGetString_MRE2() {
+    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
+    bundle.getString("key1");
+  }
+
+  /**
+   * Test method for {@link XMLResourceBundle}.
+   */
+  @Test
+  public void testGetString_English() {
+    Locale.setDefault(Locale.ENGLISH);
+    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
+    assertThat(bundle.getString("key")).isEqualTo("content");
+  }
+
+  /**
+   * Test method for {@link XMLResourceBundle}.
+   */
+  @Test
+  public void testGetString_German() {
+    Locale.setDefault(Locale.GERMAN);
+    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
+    assertThat(bundle.getString("key")).isEqualTo("Inhalt");
+    assertThat(bundle.getString("key")).isEqualTo("Inhalt");
+  }
+
+  /**
+   * Test method for {@link XMLResourceBundle#XMLResourceBundle(java.io.InputStream)}.
+   */
+  @Test(expected = IOException.class)
+  public void testXMLResourceBundle() throws IOException {
+    new XMLResourceBundle(null);
+  }
+
+  /**
+   * Test method for {@link XMLResourceBundle#getKeys()}.
+   */
+  @Test
+  public void testGetKeys() throws IOException {
+    Locale.setDefault(Locale.ENGLISH);
+    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
+    assertThat(Collections.list(bundle.getKeys())).containsOnly("key", "yet another key", "other key");
+  }
+}
