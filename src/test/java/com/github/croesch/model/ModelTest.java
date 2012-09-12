137f1dc40d2f587da12439ea5865710536d4109a
croesch <christianroesch@gmx.net>
2012-09-12 14:28:36 +0200

    #2 added some tests

--- src/test/java/com/github/croesch/model/ModelTest.java
+++ src/test/java/com/github/croesch/model/ModelTest.java
@@ -60,6 +60,107 @@ public class ModelTest implements IView {
     assertThat(this.updateCalled).isTrue();
   }
 
+  @Test
+  public final void testGetParticipant() {
+    final Participant p = new Participant("lastname",
+                                          "firstname",
+                                          Gender.MALE,
+                                          Denomination.MUSLIM,
+                                          new Date(),
+                                          "street",
+                                          13245,
+                                          "city",
+                                          CountyCouncil.COUNTY_KIRCHHEIMBOLANDEN);
+    this.model.setView(null);
+
+    this.model.store(p);
+    assertThat(this.model.getParticipant(p.getId())).isNotSameAs(p);
+    assertThat(this.model.getParticipant(p.getId())).isEqualTo(p);
+  }
+
+  @Test
+  public final void testGetListOfParticipants() {
+    final Participant p1 = new Participant("lastname",
+                                           "firstname",
+                                           Gender.MALE,
+                                           Denomination.MUSLIM,
+                                           new Date(),
+                                           "street",
+                                           13245,
+                                           "city",
+                                           CountyCouncil.COUNTY_KIRCHHEIMBOLANDEN);
+    final Participant p2 = new Participant("mustermann",
+                                           "marianne",
+                                           Gender.FEMALE,
+                                           Denomination.FREE_CHURCH,
+                                           new Date(),
+                                           "musterstreet",
+                                           11245,
+                                           "mustercity",
+                                           CountyCouncil.COUNTY_KUSEL);
+    this.model.setView(null);
+
+    assertThat(this.model.getListOfParticipants()).isEmpty();
+
+    this.model.store(p1);
+    this.model.store(p2);
+
+    assertThat(this.model.getListOfParticipants()).containsOnly(p1, p2);
+
+    this.model.deleteParticipant(p1.getId());
+    assertThat(this.model.getListOfParticipants()).containsOnly(p2);
+
+    this.model.deleteParticipant(p1.getId());
+    assertThat(this.model.getListOfParticipants()).containsOnly(p2);
+
+    this.model.deleteParticipant(p2.getId());
+    assertThat(this.model.getListOfParticipants()).isEmpty();
+  }
+
+  @Test
+  public final void testGetListOfCamps() {
+    final Participant p = new Participant("lastname",
+                                          "firstname",
+                                          Gender.MALE,
+                                          Denomination.MUSLIM,
+                                          new Date(),
+                                          "street",
+                                          13245,
+                                          "city",
+                                          CountyCouncil.COUNTY_KIRCHHEIMBOLANDEN);
+    final Camp c1 = new Camp("name", new Date(12000), new Date(2900000), "place", "rate");
+    final Camp c2 = new Camp("mustername", new Date(2000), new Date(12900000), "musterplace", "the-rate");
+    this.model.setView(null);
+
+    assertThat(this.model.getListOfCamps()).isEmpty();
+
+    this.model.store(p);
+    this.model.store(c1);
+    this.model.store(c2);
+
+    assertThat(this.model.getListOfCamps()).containsOnly(c1, c2);
+
+    this.model.deleteCamp(c1.getId());
+    assertThat(this.model.getListOfCamps()).containsOnly(c2);
+
+    this.model.deleteCamp(c1.getId());
+    assertThat(this.model.getListOfCamps()).containsOnly(c2);
+
+    this.model.deleteCamp(c2.getId());
+    assertThat(this.model.getListOfCamps()).isEmpty();
+  }
+
+  @Test
+  public final void testGetCamp() {
+    final Camp c = new Camp("name", new Date(12000), new Date(2900000), "place", "rate");
+
+    this.model.setView(null);
+    this.model.store(c);
+
+    assertThat(this.model.getCamp(c.getId())).isNotSameAs(c);
+    assertThat(this.model.getCamp(c.getId())).isEqualTo(c);
+  }
+
   /**
    * Test method for {@link Model#store(Camp)}.
    */
