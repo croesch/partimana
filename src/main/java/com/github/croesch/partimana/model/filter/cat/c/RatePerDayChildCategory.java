502cf9f546d5278cc970bc2a1092c10a970852cf
croesch <christianroesch@gmx.net>
2012-10-27 22:08:26 +0200

    #14 wrote test for rate-per-day category of camp

--- src/main/java/com/github/croesch/partimana/model/filter/cat/c/RatePerDayChildCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/c/RatePerDayChildCategory.java
@@ -1,24 +1,28 @@
 package com.github.croesch.partimana.model.filter.cat.c;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.StringCategory;
 import com.github.croesch.partimana.types.Camp;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>rate per day</em> of a {@link Camp}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class RatePerDayChildCategory extends StringCategory<Camp> {
+public final class RatePerDayChildCategory extends StringCategory<Camp> {
 
+  /**
+   * Constructs the category that describes the attribute <em>name</em> of a {@link Camp}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public RatePerDayChildCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_CAMP_RATE_PER_DAY.text());
   }
 
   @Override
   protected String getValue(final Camp element) {
     return element.getRatePerDayChildren();
   }
-
 }
---
+++ src/test/java/com/github/croesch/partimana/model/filter/cat/c/RatePerDayChildCategoryTest.java
@@ -0,0 +1,84 @@
+package com.github.croesch.partimana.model.filter.cat.c;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.croesch.partimana.i18n.Text;
+import com.github.croesch.partimana.model.api.IFilterType;
+import com.github.croesch.partimana.model.filter.types.Contains;
+import com.github.croesch.partimana.model.filter.types.EndsWith;
+import com.github.croesch.partimana.model.filter.types.Equals;
+import com.github.croesch.partimana.model.filter.types.EqualsIgnoreCase;
+import com.github.croesch.partimana.model.filter.types.NotEquals;
+import com.github.croesch.partimana.model.filter.types.NotEqualsIgnoreCase;
+import com.github.croesch.partimana.model.filter.types.StartsWith;
+import com.github.croesch.partimana.types.Camp;
+
+/**
+ * Provides test cases for {@link RatePerDayChildCategory}.
+ * 
+ * @author croesch
+ * @since Date: Oct 27, 2012
+ */
+public class RatePerDayChildCategoryTest {
+
+  private RatePerDayChildCategory category;
+
+  @Before
+  public void setUp() {
+    this.category = new RatePerDayChildCategory();
+  }
+
+  @Test
+  public void testGetFilters() {
+    assertThat(this.category.getFilterTypes()).containsOnly(new Equals<Object>(), new NotEquals<Object>(),
+                                                            new StartsWith(), new EndsWith(), new EqualsIgnoreCase(),
+                                                            new NotEqualsIgnoreCase(), new Contains());
+
+    final IFilterType<String> filterType = this.category.getFilterTypes().get(0);
+    filterType.setFilterValue("12345");
+
+    assertThat(this.category.getFilterTypes().contains(filterType)).isFalse();
+  }
+
+  @Test
+  public void testSetAndGetFilter() {
+    assertThat(this.category.getFilter()).isNull();
+    this.category.setFilter(new StartsWith());
+    assertThat(this.category.getFilter()).isEqualTo(new StartsWith());
+    this.category.setFilter(null);
+    assertThat(this.category.getFilter()).isNull();
+  }
+
+  @Test
+  public void testGetShortDescription() {
+    assertThat(this.category.getShortDescription()).isEqualTo(Text.FILTER_CAT_CAMP_RATE_PER_DAY.text());
+  }
+
+  @Test
+  public void isMatchingFilter() {
+    assertThat(this.category.isMatchingFilter(null)).isFalse();
+
+    final Camp c = new Camp("OFZ", new Date(5000000), new Date(10000000), "dort", "20");
+    c.setRatePerDayChildren("12");
+    assertThat(this.category.isMatchingFilter(c)).isFalse();
+
+    final Contains contains = new Contains();
+    contains.setFilterValue("12");
+    this.category.setFilter(contains);
+    assertThat(this.category.isMatchingFilter(c)).isTrue();
+
+    final EndsWith endsWith = new EndsWith();
+    endsWith.setFilterValue("I2");
+    this.category.setFilter(endsWith);
+    assertThat(this.category.isMatchingFilter(c)).isFalse();
+
+    c.setRatePerDayChildren(null);
+    this.category.setFilter(contains);
+    assertThat(this.category.isMatchingFilter(c)).isFalse();
+  }
+}
