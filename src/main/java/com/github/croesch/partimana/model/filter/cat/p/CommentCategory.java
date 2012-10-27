cb21481fccec107dce2df84a9151e5bb3be8553d
croesch <christianroesch@gmx.net>
2012-10-28 01:11:27 +0200

    #14 wrote test for comment category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/CommentCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/CommentCategory.java
@@ -1,23 +1,28 @@
 package com.github.croesch.partimana.model.filter.cat.p;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.StringCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>comment</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class CommentCategory extends StringCategory<Participant> {
+public final class CommentCategory extends StringCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>comment</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public CommentCategory() {
-    super(null);//TODO
+    super(Text.FILTER_CAT_PARTICIPANT_COMMENT);
   }
 
   @Override
   protected String getValue(final Participant element) {
     return element.getComment();
   }
-
 }
