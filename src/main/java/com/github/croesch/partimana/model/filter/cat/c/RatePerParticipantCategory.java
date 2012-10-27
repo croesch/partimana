b966f96f628ebc42bf2b104f5274a999fa26316f
croesch <christianroesch@gmx.net>
2012-10-27 22:10:57 +0200

    #14 wrote test for rate-per-participant category of camp

--- src/main/java/com/github/croesch/partimana/model/filter/cat/c/RatePerParticipantCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/c/RatePerParticipantCategory.java
@@ -1,19 +1,24 @@
 package com.github.croesch.partimana.model.filter.cat.c;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.StringCategory;
 import com.github.croesch.partimana.types.Camp;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>rate per participant</em> of a {@link Camp}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class RatePerParticipantCategory extends StringCategory<Camp> {
+public final class RatePerParticipantCategory extends StringCategory<Camp> {
 
+  /**
+   * Constructs the category that describes the attribute <em>name</em> of a {@link Camp}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public RatePerParticipantCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_CAMP_RATE_PER_PART.text());
   }
 
   @Override
