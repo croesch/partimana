318c8902107d9c9ccc1e0b516f7185c781a7ae24
croesch <christianroesch@gmx.net>
2012-10-28 00:00:06 +0200

    #14 wrote test for bank-code-number category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/BankCodeNumberCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/BankCodeNumberCategory.java
@@ -1,24 +1,28 @@
 package com.github.croesch.partimana.model.filter.cat.p;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.IntegerCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>bank code number</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class BankCodeNumberCategory extends IntegerCategory<Participant> {
+public final class BankCodeNumberCategory extends IntegerCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>bank code number</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public BankCodeNumberCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_PARTICIPANT_BANK_CODE_NUM);
   }
 
   @Override
   protected Integer getValue(final Participant element) {
     return element.getBankCodeNumber();
   }
-
 }
---
+++ src/test/java/com/github/croesch/partimana/model/filter/cat/p/BankCodeNumberCategoryTest.java
@@ -0,0 +1,88 @@
+package com.github.croesch.partimana.model.filter.cat.p;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.croesch.partimana.i18n.Text;
+import com.github.croesch.partimana.model.api.IFilterType;
+import com.github.croesch.partimana.model.filter.types.Equals;
+import com.github.croesch.partimana.model.filter.types.GreaterThan;
+import com.github.croesch.partimana.model.filter.types.LessThan;
+import com.github.croesch.partimana.model.filter.types.NotEquals;
+import com.github.croesch.partimana.types.CountyCouncil;
+import com.github.croesch.partimana.types.Denomination;
+import com.github.croesch.partimana.types.Gender;
+import com.github.croesch.partimana.types.Participant;
+
+/**
+ * Provides test cases for {@link BankCodeNumberCategory}.
+ * 
+ * @author croesch
+ * @since Date: Oct 27, 2012
+ */
+public class BankCodeNumberCategoryTest {
+
+  private BankCodeNumberCategory category;
+
+  @Before
+  public void setUp() {
+    this.category = new BankCodeNumberCategory();
+  }
+
+  @Test
+  public void testGetFilters() {
+    assertThat(this.category.getFilterTypes()).containsOnly(new Equals<Object>(), new NotEquals<Object>(),
+                                                            new LessThan(), new GreaterThan());
+
+    final IFilterType<Integer> filterType = this.category.getFilterTypes().get(0);
+    filterType.setFilterValue(12345);
+
+    assertThat(this.category.getFilterTypes().contains(filterType)).isFalse();
+  }
+
+  @Test
+  public void testSetAndGetFilter() {
+    assertThat(this.category.getFilter()).isNull();
+    this.category.setFilter(new Equals<Integer>());
+    assertThat(this.category.getFilter()).isEqualTo(new Equals<Integer>());
+    this.category.setFilter(null);
+    assertThat(this.category.getFilter()).isNull();
+  }
+
+  @Test
+  public void testGetShortDescription() {
+    assertThat(this.category.getShortDescription()).isEqualTo(Text.FILTER_CAT_PARTICIPANT_BANK_CODE_NUM.text());
+    assertThat(this.category.getShortDescription()).isEqualTo(Text.PARTICIPANT_BANK_CODE_NUMBER.text());
+  }
+
+  @Test
+  public void isMatchingFilter() {
+    assertThat(this.category.isMatchingFilter(null)).isFalse();
+
+    final Participant p = new Participant("Maxi",
+                                          "Musterfrau",
+                                          Gender.FEMALE,
+                                          Denomination.CATHOLIC,
+                                          new Date(100000000),
+                                          "street",
+                                          12,
+                                          "city",
+                                          CountyCouncil.COUNTY_RHEIN_PFALZ);
+    p.setBankCodeNumber(123456789);
+    assertThat(this.category.isMatchingFilter(p)).isFalse();
+
+    final LessThan less = new LessThan();
+    less.setFilterValue(500);
+    this.category.setFilter(less);
+    assertThat(this.category.isMatchingFilter(p)).isFalse();
+
+    final GreaterThan greater = new GreaterThan();
+    greater.setFilterValue(500);
+    this.category.setFilter(greater);
+    assertThat(this.category.isMatchingFilter(p)).isTrue();
+  }
+}
