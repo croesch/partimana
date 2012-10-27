641d67c3a7e75bed7c33e3aa92f255dd9ee1861e
croesch <christianroesch@gmx.net>
2012-10-27 23:54:13 +0200

    #14 wrote test for bank-account-number category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/BankAccountNumberCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/BankAccountNumberCategory.java
@@ -1,24 +1,28 @@
 package com.github.croesch.partimana.model.filter.cat.p;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.IntegerCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>bank account number</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class BankAccountNumberCategory extends IntegerCategory<Participant> {
+public final class BankAccountNumberCategory extends IntegerCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>bank account number</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public BankAccountNumberCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_PARTICIPANT_BANK_ACC_NUM);
   }
 
   @Override
   protected Integer getValue(final Participant element) {
     return element.getBankAccountNumber();
   }
-
 }
