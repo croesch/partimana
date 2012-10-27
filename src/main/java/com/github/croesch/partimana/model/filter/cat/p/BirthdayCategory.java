80e5dc84f3179915d1737cd77616797b8769e19d
croesch <christianroesch@gmx.net>
2012-10-28 00:21:58 +0200

    #14 wrote test for birthday category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/BirthdayCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/BirthdayCategory.java
@@ -2,20 +2,25 @@ package com.github.croesch.partimana.model.filter.cat.p;
 
 import java.util.Date;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.DateCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>birthday</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class BirthdayCategory extends DateCategory<Participant> {
+public final class BirthdayCategory extends DateCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>birthday</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public BirthdayCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_PARTICIPANT_BIRTHDAY);
   }
 
   @Override
