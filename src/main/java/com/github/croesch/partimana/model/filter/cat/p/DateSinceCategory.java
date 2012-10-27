2bfde1db4c8483868c5f9dc3516fad9282da356e
croesch <christianroesch@gmx.net>
2012-10-28 01:31:11 +0200

    #14 wrote test for date since in db category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/DateSinceCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/DateSinceCategory.java
@@ -2,25 +2,29 @@ package com.github.croesch.partimana.model.filter.cat.p;
 
 import java.util.Date;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.DateCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>date since in db</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class DateSinceCategory extends DateCategory<Participant> {
+public final class DateSinceCategory extends DateCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>date since in db</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public DateSinceCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_PARTICIPANT_DATE_SINCE);
   }
 
   @Override
   protected Date getValue(final Participant element) {
     return element.getDateSinceInDataBase();
   }
-
 }
