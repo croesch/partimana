47a59b0eefa2621013c777a0578286d6a0402187
croesch <christianroesch@gmx.net>
2012-10-28 00:14:44 +0200

    #14 wrote test for bank-name category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/BankNameCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/BankNameCategory.java
@@ -1,18 +1,24 @@
 package com.github.croesch.partimana.model.filter.cat.p;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.StringCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>bank name</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class BankNameCategory extends StringCategory<Participant> {
+public final class BankNameCategory extends StringCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>bank name</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public BankNameCategory() {
-    super(null);//TODO
+    super(Text.FILTER_CAT_PARTICIPANT_BANK_NAME);
   }
 
   @Override
