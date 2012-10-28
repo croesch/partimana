8aa78334d87a501c19ecd5aa938948dd26a356cb
croesch <christianroesch@gmx.net>
2012-10-28 02:45:51 +0200

    #14 wrote test for the phone category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/PhoneCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/PhoneCategory.java
@@ -1,23 +1,28 @@
 package com.github.croesch.partimana.model.filter.cat.p;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.StringCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>phone</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class PhoneCategory extends StringCategory<Participant> {
+public final class PhoneCategory extends StringCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>phone</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public PhoneCategory() {
-    super(null);//TODO
+    super(Text.FILTER_CAT_PARTICIPANT_PHONE);
   }
 
   @Override
   protected String getValue(final Participant element) {
     return element.getPhone();
   }
-
 }
