7bffdbd2f44f8dda540c2dfcf74ef6308eb7589d
croesch <christianroesch@gmx.net>
2012-10-28 02:01:33 +0100

    #14 wrote test for the postal city category of participant

--- src/main/java/com/github/croesch/partimana/model/filter/cat/p/PostalCityCategory.java
+++ src/main/java/com/github/croesch/partimana/model/filter/cat/p/PostalCityCategory.java
@@ -1,24 +1,28 @@
 package com.github.croesch.partimana.model.filter.cat.p;
 
+import com.github.croesch.partimana.i18n.Text;
 import com.github.croesch.partimana.model.filter.cat.StringCategory;
 import com.github.croesch.partimana.types.Participant;
 
 /**
- * TODO Comment here ...
+ * The category that describes the attribute <em>postal city</em> of a {@link Participant}.
  * 
  * @author croesch
  * @since Date: Oct 20, 2012
  */
-public class PostalCityCategory extends StringCategory<Participant> {
+public final class PostalCityCategory extends StringCategory<Participant> {
 
+  /**
+   * Constructs the category that describes the attribute <em>postal city</em> of a {@link Participant}.
+   * 
+   * @since Date: Oct 27, 2012
+   */
   public PostalCityCategory() {
-    super(null);
-    // TODO Auto-generated constructor stub
+    super(Text.FILTER_CAT_PARTICIPANT_POSTAL_CITY);
   }
 
   @Override
   protected String getValue(final Participant element) {
     return element.getCityPostal();
   }
-
 }
