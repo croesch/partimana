8a580b89081a4811e9f400e2be79a9e10a939163
croesch <christianroesch@gmx.net>
2012-10-28 02:04:02 +0100

    #14 wrote test for the postal street category of participant

---
+++ src/test/java/com/github/croesch/partimana/model/filter/cat/p/PostalStreetCategoryTest.java
@@ -0,0 +1,93 @@
+package com.github.croesch.partimana.model.filter.cat.p;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.croesch.partimana.i18n.Text;
+import com.github.croesch.partimana.model.api.IFilterType;
+import com.github.croesch.partimana.model.filter.types.Contains;
+import com.github.croesch.partimana.model.filter.types.EndsWith;
+import com.github.croesch.partimana.model.filter.types.Equals;
+import com.github.croesch.partimana.model.filter.types.EqualsIgnoreCase;
+import com.github.croesch.partimana.model.filter.types.NotEquals;
+import com.github.croesch.partimana.model.filter.types.NotEqualsIgnoreCase;
+import com.github.croesch.partimana.model.filter.types.StartsWith;
+import com.github.croesch.partimana.types.CountyCouncil;
+import com.github.croesch.partimana.types.Denomination;
+import com.github.croesch.partimana.types.Gender;
+import com.github.croesch.partimana.types.Participant;
+
+/**
+ * Provides test cases for {@link PostalStreetCategory}.
+ * 
+ * @author croesch
+ * @since Date: Oct 27, 2012
+ */
+public class PostalStreetCategoryTest {
+
+  private PostalStreetCategory category;
+
+  @Before
+  public void setUp() {
+    this.category = new PostalStreetCategory();
+  }
+
+  @Test
+  public void testGetFilters() {
+    assertThat(this.category.getFilterTypes()).containsOnly(new Equals<Object>(), new NotEquals<Object>(),
+                                                            new StartsWith(), new EndsWith(), new EqualsIgnoreCase(),
+                                                            new NotEqualsIgnoreCase(), new Contains());
+
+    final IFilterType<String> filterType = this.category.getFilterTypes().get(0);
+    filterType.setFilterValue("12345");
+
+    assertThat(this.category.getFilterTypes().contains(filterType)).isFalse();
+  }
+
+  @Test
+  public void testSetAndGetFilter() {
+    assertThat(this.category.getFilter()).isNull();
+    this.category.setFilter(new StartsWith());
+    assertThat(this.category.getFilter()).isEqualTo(new StartsWith());
+    this.category.setFilter(null);
+    assertThat(this.category.getFilter()).isNull();
+  }
+
+  @Test
+  public void testGetShortDescription() {
+    assertThat(this.category.getShortDescription()).isEqualTo(Text.FILTER_CAT_PARTICIPANT_POSTAL_STREET.text());
+    assertThat(this.category.getShortDescription()).isEqualTo(Text.STREET.text() + " ("
+                                                                      + Text.PARTICIPANT_ADDRESS_POSTAL.text() + ")");
+  }
+
+  @Test
+  public void isMatchingFilter() {
+    assertThat(this.category.isMatchingFilter(null)).isFalse();
+
+    final Participant p = new Participant("Musterfrau",
+                                          "Maxi",
+                                          Gender.FEMALE,
+                                          Denomination.CATHOLIC,
+                                          new Date(100000000),
+                                          "street",
+                                          12,
+                                          "city",
+                                          CountyCouncil.COUNTY_RHEIN_PFALZ);
+    p.setStreetPostal("the street");
+    assertThat(this.category.isMatchingFilter(p)).isFalse();
+
+    final StartsWith startsWith = new StartsWith();
+    startsWith.setFilterValue("the ");
+    this.category.setFilter(startsWith);
+    assertThat(this.category.isMatchingFilter(p)).isTrue();
+
+    final EndsWith endsWith = new EndsWith();
+    endsWith.setFilterValue("sTreet");
+    this.category.setFilter(endsWith);
+    assertThat(this.category.isMatchingFilter(p)).isFalse();
+  }
+}
