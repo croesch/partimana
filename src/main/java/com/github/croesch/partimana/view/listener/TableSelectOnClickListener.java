0478cbe7116b3a013a53f56fdbf897a09f643365
croesch <christianroesch@gmx.net>
2013-03-17 14:17:37 +0100

    #22 implemented listener to select cells regardless the mouse button

---
+++ src/main/java/com/github/croesch/partimana/view/listener/TableSelectOnClickListener.java
@@ -0,0 +1,25 @@
+package com.github.croesch.partimana.view.listener;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTable;
+
+/**
+ * A mouse listener that selects the row of the table when right click happened.
+ * 
+ * @author croesch
+ * @since Date: Mar 17, 2013
+ */
+public final class TableSelectOnClickListener extends MouseAdapter {
+
+  @Override
+  public void mouseReleased(final MouseEvent e) {
+    if (e.getSource() instanceof JTable) {
+      final JTable table = (JTable) e.getSource();
+      final int row = table.rowAtPoint(e.getPoint());
+
+      table.setRowSelectionInterval(row, row);
+    }
+  }
+}
